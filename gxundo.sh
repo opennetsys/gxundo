ac437c7d127e1ab295db88ba25dec71076bf1394
Miguel Mota <miguelmota2@gmail.com>
2018-07-05 14:45:15 -0700

    replace recursively

--- gxundo.sh
+++ gxundo.sh
@@ -35,18 +35,21 @@ echo "finding imports..."
 echo "path $dirpath"
 
 # read all Go imports that contain "gx/ipfs/"
-for line in $(find "$dirpath" -maxdepth 10 -type d | xargs -I '{}' go list -json './{}...' | jq '.Imports' | grep 'gx/ipfs/' | sed -e 's/gx\///g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
+for dir in $(find "$dirpath" -maxdepth 10 -type d)
 do
-  # fetch the gx package.json and read the github url
-  new=$(curl -s "https://gateway.ipfs.io/$line/package.json" | jq '.gx.dvcsimport' | sed -e 's/"//g')
-  old="gx/$line"
+  for line in $(go list -json "./$dir/..." | jq '.Imports' | grep 'gx/ipfs/' | sed -e 's/gx\///g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
+  do
+    # fetch the gx package.json and read the github url
+    new=$(curl -s "https://gateway.ipfs.io/$line/package.json" | jq '.gx.dvcsimport' | sed -e 's/"//g')
+    old="gx/$line"
 
-  echo "$old => $new"
+    echo "$old => $new"
 
-  # replace the imports
-  gorep -path="$dirpath" \
-      -from="$old" \
-      -to="$new"
+    # replace the imports
+    gorep -path="$dir" \
+        -from="$old" \
+        -to="$new"
+  done
 done
 
 echo "complete"
