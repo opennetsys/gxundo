9fb08d3432b110cb519a993eb7fe1aee883392e3
Miguel Mota <miguelmota2@gmail.com>
2018-07-05 13:57:58 -0700

    add comments

--- gxundo.sh
+++ gxundo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# function to install `jq`
 function install_jq() {
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
       sudo apt-get install jq
@@ -8,18 +9,21 @@ function install_jq() {
   fi
 }
 
+# check if `gorep` is installed, otherwise install it.
 command -v gorep >/dev/null
 if [ $? -ne 0 ]; then
   echo "gorep is not installed. Installing..."
   go get github.com/novalagung/gorep && echo "gorep installed"
 fi
 
+# check if `jq` is installed, otherwise install it.
 command -v jq >/dev/null
 if [ $? -ne 0 ]; then
   echo "jq is not installed. Installing..."
   install_jq && echo "jq installed"
 fi
 
+# the path to search passed an the only argument
 dirpath="$1"
 
 if [[ "$dirpath" == "" ]]; then
@@ -30,13 +34,16 @@ fi
 echo "finding imports..."
 echo "path $dirpath"
 
-for line in $(go list  -json ./...  | jq '.Imports' | grep 'gx/ipfs/' | sed -e 's/gx\///g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
+# read all Go imports that contain "gx/ipfs/"
+for line in $(go list  -json ./...  | jq '.Imports' | grep 'gx/ipfs\//' | sed -e 's/gx\///g' | sed -e 's/"//g' | sed -e 's/,//g' | sed -e 's/ //g')
 do
-  old="gx/$line"
+  # fetch the gx package.json and read the github url
   new=$(curl -s "https://gateway.ipfs.io/$line/package.json" | jq '.gx.dvcsimport' | sed -e 's/"//g')
+  old="gx/$line"
 
   echo "$old => $new"
 
+  # replace the imports
   gorep -path="$dirpath" \
       -from="$old" \
       -to="$new"
